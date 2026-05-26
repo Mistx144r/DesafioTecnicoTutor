c94814e31caf1c05624839660fecb3452fdcf881
Lucas <lucasdmendoncaa@gmail.com>
2026-05-25 21:42:02 -0300

    fix: Troquei o CORS hardcoded para uma approach mais segura em produção.

--- backend/index.php
+++ backend/index.php
@@ -25,11 +25,17 @@ register_shutdown_function(function () {
 
 date_default_timezone_set('America/Sao_Paulo');
 
+$allowedOrigin = $_ENV['ALLOWED_ORIGIN'] ?? '';
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if ($origin === $allowedOrigin) {
+    header("Access-Control-Allow-Origin: $origin");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Content-Type');
+}
+
 header('Content-Type: application/json'); // Por padrão PHP retorna HTML, então para esse desafio precisei mudar o retorno para JSON.
-header('Access-Control-Allow-Origin: http://localhost:5173');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type');
 
 $uri    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
